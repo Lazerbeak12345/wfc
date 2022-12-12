91b89e13aa55afdf6b5796d842395d532023ec85
Nathan Fritzler <nfblaster@live.com>
2022-12-11 17:54:45 -0700

    Bunches of general touchup.
    How did I forget to do some of these things lol?

--- generic.lua
+++ generic.lua
@@ -4,9 +4,11 @@ function solver.new(init)
 		get_hitbox = init.get_hitbox,
 		bits = {
 			band = init.bits.band,
+			bor = init.bits.bor,
 			empty = init.bits.empty
 		},
-		set = init.set
+		set = init.set,
+		table = init.table
 	}
 	setmetatable(self, solver)
 	return self
@@ -16,26 +18,36 @@ function solver:_full_queue(size)
 	local output = self.set.new()
 	while index < size do
 		output = output:add(index)
+		index = index + 1
 	end
 	return output
+end
+function solver:state_to_acceptable(value, vec)
+	-- Super slow. Memoize this?
+	local acceptable = self.bits.empty()
+	for id, relation in ipairs(self.table) do
+		if self.bits.index(value, id) and relation[vec] then
+			acceptable = self.bits.bor(acceptable, relation[vec])
+		end
+	end
 end
 function solver:get_possible_states(state, location)
 	local nearby = self:get_hitbox(location)
 	local vals = state[location] -- old value
 	for _, l2 in ipairs(nearby) do
-		vals = self.bits["and"](vals, state[l2])
+		vals = self.bits.band(
+			vals,
+			solver:state_to_acceptable(
+				state[l2],
+				l2 - location  -- A vector. In 1D. Super easy to scale up to `n` Ds. If youv'e done cellular automata before. Wimps.
+			)
+		)
 	end
 	return vals, nearby
 end
 function solver:reduce(state, location)
-	local possible, nearby = self:get_possible_states(
-		state,
-		location
-		)
-	local new_states = self.bits["and"](
-		state[location],
-		possible
-		)
+	local possible, nearby = self:get_possible_states(state, location)
+	local new_states = self.bits.band(state[location], possible)
 	state[location] = new_states
 	if new_states == state then
 		return {}
